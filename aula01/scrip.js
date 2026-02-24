29e1e520b1434d4c8595a9741a7f25e5dff22636
htesther <estervitoriaribeiro73@gmail.com>
2026-02-24 11:36:56 -0300

    24/02

---
+++ aula01/scrip.js
@@ -0,0 +1,21 @@
+var nome = Esther
+var idade = 16
+var altura = 1.60
+
+console.log(nome);
+console.log(idade);
+console.log(altura);
+
+document.write("<h1>Olá, seja bem vindo ao JavaScript</h1>");
+
+document.write(nome + "<br>");
+document.write(idade + "<br>");
+document.write(altura + "<br>");
+
+var dias = idade * 365;
+
+document.write("<p style= 'color : red'>Você Já Viveu " + dias + " dias</p>");
+
+
+
+
